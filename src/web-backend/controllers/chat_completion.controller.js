6ac7bf1ecac4eae1d758848840150df60b8fcdb9
Aaron Aco <aaronacodev@gmail.com>
2024-05-08 15:23:18 +0800

    add html and css files instructions

--- src/web-backend/controllers/chat_completion.controller.js
+++ src/web-backend/controllers/chat_completion.controller.js
@@ -1,37 +1,84 @@
 import OpenAIHelper from "../helpers/openai.helper.js";
-
+import fs from "fs"
 class ChatCompletion {
     constructor() {
     }
 
+    readFileAsync(filePath) {
+        return new Promise((resolve, reject) => {
+            fs.readFile(filePath, 'utf8', (err, data) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(data);
+                }
+            });
+        });
+    }
     /* TODO : ADD CSS FILE TO CHECK */
     chat = async (req, res) => {
         let response_data = { status: false, result: "", message: "", error: null };
         try {
-            let html_file = req.files[0].path; //html depends
 
+            let upload_files = req.files; //html depends
+            console.log("upload_files", upload_files);
+            let html_upload_file = upload_files[0].originalname.indexOf(".html") > 0 ? upload_files[0].path : upload_files[1].path; //html depends
+            let css_upload_file = upload_files[0].originalname.indexOf(".css") > 0 ? upload_files[0].path : upload_files[1].path; //html depends
 
-            if (html_file) {
+            if (html_upload_file && css_upload_file) {
+                let html_file = await this.readFileAsync(html_upload_file);
+                let css_file = await this.readFileAsync(css_upload_file);
                 let openAIHelper = new OpenAIHelper();
+
+                let system_prompt = `
+                You are CodeReviewBot, an automated checker to review the code given by the user.
+                Review the code and ensure it adheres to the provided instructions. You should meticulously examine the text, checking for compliance with each instruction. Thoroughness and accuracy are crucial in this review process. Use bullet points to clearly outline the changes needed to help organize information more effectively, making it easier for the reader to understand and follow. Specify the name of the file to make the changes. Always specify the code to change. Additionally, avoid directly mentioning the instructions, but rather explain the reasons behind the suggested improvements to foster clarity and encourages comprehension. Do not include a modified code. If the code follows all the instructions, simplay say 'All good'
+                `
+
+                let html_instructions_prompt = `
+                HTML Instructions:	
+                -Use Lowercase: Although HTML is case-insensitive, it's a good practice to use lowercase for HTML tags and attributes for consistency and readability.	
+                -Use Semantic Tags: Use semantic HTML tags to structure the content logically.	
+                -Use Proper Indentation.
+                -Avoid Inline Styles: Separate the styling into an external CSS file.	
+                -Avoid Redundant Nested Tags: Avoid unnecessary nesting of HTML tags. Each tag should serve a specific purpose, and nesting should be kept to a minimum to maintain simplicity and clarity.	
+                -Avoid text hanging. Enclose all text content within appropriate HTML tags to maintain a structured document.	
+                -Readable id and class name for each HTML elements	
+                `
+
+                let css_instructions_prompt = `
+                CSS Instructions:	
+                -Choose descriptive and meaningful names for IDs and classes.	
+                -Use Semicolons: End each CSS property declaration with a semicolon to ensure proper syntax	
+                -Organize Stylesheets: Structure the CSS code logically and consistently. One common approach is to group styles by element or module.	
+                -Alphabetical Order: Arrange the  CSS properties alphabetically within each selector. This makes it easier to find and manage properties.	
+                -Avoid Duplicate IDs and Classes.	
+                -Avoid Unused CSS: Remove any CSS rules that are not being used in the HTML. Unused CSS adds unnecessary file size and can clutter the stylesheet.	
+                `
                 let messages = [
                     {
                         role: "system",
-                        content: `
-                            You are CodeReviewBot, an automated checker to review the code given by the user. \
-                            Use HTML5 Structure: Start with the basic structure of an HTML5 document.	\
-                            Use Lowercase: Although HTML is case-insensitive, it's a good practice to use lowercase for HTML tags and attributes for consistency and readability.	 \
-                            Use Semantic Tags: Use semantic HTML tags to structure the content logically.	 \
-                            Use Proper Indentation of 4 spaces or 1 tab	 \
-                            Use Comments: Add comments to explain different sections of the code: \	
-                            Avoid Inline Styles: Separate the styling into an external CSS file. \
-                            Avoid Redundant Nested Tags: Avoid unnecessary nesting of HTML tags. Each tag should serve a specific purpose, and nesting should be kept to a minimum to maintain simplicity and clarity. \
-                            Generate a response in a calm and friendly manner, with a focus on maintaining a positive and approachable tone. Use bullet points to clearly outline the changes needed to help organize information more effectively, making it easier for the reader to understand and follow. Additionally, avoid directly mentioning the instructions, but rather explain the reasons behind the suggested improvements to foster clarity and encourages comprehension.\
-                        `
+                        content: system_prompt
                     },
-                    { role: "user", content: `Check the following HTML code for good practices:\n\n${html_file}\n\n Suggestions for improving the HTML code:` }, // file readfile 
+                    { role: "user", content: `Check the following HTML code for good practices:\n\n${html_file}\n\n Refer to this HTML instructions: ${html_instructions_prompt}` }, // file readfile 
+                    { role: "user", content: `Check the following css code for good practices:\n\n${css_file}\n\n Refer to this HTML instructions: ${css_instructions_prompt}` }, // file readfile 
                 ]
                 response_data = await openAIHelper.chatCompletionAssistant(messages);
 
+                if (response_data.status) {
+                    const uploadedPaths = req.files.map(file => file.path);
+                
+                    // Remove each uploaded file
+                    uploadedPaths.forEach(filePath => {
+                        fs.unlink(filePath, err => {
+                            if (err) {
+                                console.error('Error removing file:', err);
+                            } else {
+                                console.log('File removed successfully:', filePath);
+                            }
+                        });
+                    });
+                }
                 console.log(response_data)
             }
         }
